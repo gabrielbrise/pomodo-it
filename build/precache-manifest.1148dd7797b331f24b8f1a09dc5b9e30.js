6a424b84c3219dc229aa87ac9f0f30c58db710cb
Brise <gabriel_ns@yahoo.com.br>
2019-05-21 09:30:38 -0300

    Testing Github Pages

---
+++ build/precache-manifest.1148dd7797b331f24b8f1a09dc5b9e30.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d92e745b81e34c4335e1e433a728e4c",
+    "url": "/pomodo-it/index.html"
+  },
+  {
+    "revision": "fcfc12115e8d8c96dd0b",
+    "url": "/pomodo-it/static/css/main.ffa2fc18.chunk.css"
+  },
+  {
+    "revision": "7aef4bf1f6f2cb954e34",
+    "url": "/pomodo-it/static/js/2.13410fef.chunk.js"
+  },
+  {
+    "revision": "fcfc12115e8d8c96dd0b",
+    "url": "/pomodo-it/static/js/main.3b542114.chunk.js"
+  },
+  {
+    "revision": "b2de8434809ff9933d27",
+    "url": "/pomodo-it/static/js/runtime~main.4161a908.js"
+  },
+  {
+    "revision": "ef5422db318318c8d5babf3b822b7934",
+    "url": "/pomodo-it/static/media/delete.ef5422db.svg"
+  },
+  {
+    "revision": "e1133501f122580e1c82ca186f5270d3",
+    "url": "/pomodo-it/static/media/logo.e1133501.png"
+  },
+  {
+    "revision": "cd7da8761a8b342ce77eca28d43559c7",
+    "url": "/pomodo-it/static/media/pause.cd7da876.svg"
+  },
+  {
+    "revision": "0afc49f2d50b5890c9b7b82d45d889f3",
+    "url": "/pomodo-it/static/media/play.0afc49f2.svg"
+  },
+  {
+    "revision": "3d10c196a7159034be4a64ccdc71818b",
+    "url": "/pomodo-it/static/media/reset.3d10c196.svg"
+  }
+]);
